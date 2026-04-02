488345c3b28cab4d75c72dc1d94adfb3e3464c1c
Nathan Lodder <nathan.lodder@proton.me>
2026-04-02 16:12:39 -0700

    added drone stabilizer

--- stable_drone/scripts/stable_drone.py
+++ stable_drone/scripts/stable_drone.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 from drone_desc.scripts.drone_cmd_bridge import DroneCmdBridge
 import rospy
-from geometry_msgs.msg import Wrench, Twist
+from geometry_msgs.msg import Wrench
 from sensor_msgs.msg import Imu
-from std_msgs.msg import Float64
+from gazebo_msgs.srv import ApplyBodyWrench
 
 class Stabilizer:
     def __init__(self):
         rospy.init_node('stabilizer')
         self.UPDATE_HZ = 60
+        self.P = 0.1
 
         # latch last command
         self.current_wrench = Wrench()
@@ -16,21 +17,24 @@ class Stabilizer:
         # subscribe to drone's imu topic
         rospy.Subscriber("imu", Imu, self.imu_callback)
         # these will become dronex/planar_cmd and dronex/vert_cmd from launch namespacing
-        self.ang_pub = rospy.Publisher("ang_vel", Twist, queue_size=1)
+        # self.ang_pub = rospy.Publisher("ang_force", Wrench, queue_size=1)
     
     def imu_callback(self, msg):
-        self.current_wrench = msg
+        # simple P controller to stabilize drone's orientation
+        # this is a placeholder for your actual control algorithm
+        self.current_wrench.torque.x = -self.P * msg.angular_velocity.x
+        self.current_wrench.torque.y = -self.P * msg.angular_velocity.y
+        self.current_wrench.torque.z = -self.P * msg.angular_velocity.z
+        print(f"IMU data: {msg.angular_velocity}, Published wrench: {self.current_wrench}")
     
-    def run(self):
-        # rate of cmd updates to planar and vertical movement plugins
-        rate = rospy.Rate(self.UPDATE_HZ)
-        while not rospy.is_shutdown():
-            self.mov_pub.publish(self.current_wrench)
-            rate.sleep()
+        rospy.wait_for_service('/gazebo/apply_body_wrench')
+        try:
+            apply_wrench = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
+            apply_wrench(body_name=f"{self.ns}::link_drone_body", wrench=self.current_wrench, duration=rospy.Duration(0.1))
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
 if __name__ == '__main__':
-    try:
-        bridge = DroneCmdBridge()
-        bridge.run()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('stabilizer')
+    stabilizer = Stabilizer()
+    rospy.spin()
